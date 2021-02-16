a53da350cd8e3778de611fdd4ced8a94c6419dfc
Gabriella Meggyes <gabriella@meggy.es>
2021-02-16 09:57:18 +0100

    initial commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,48 @@
 import React, { useState, Fragment } from 'react'
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
-const StyledHeader = styled.div`
-    position: fixed;
-    z-index: 100;
-    top: 0;
-    width: 100%;
-    padding: 0;
-    transition: all .25s ease-out;
-    background-color: #000;
-    height: 5rem;
-`;
-
-const ActionButton = styled.div`
-
+const StyledWrapper = styled.div`
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  background-color: ${props => props.theme.mainBlack};
+  transition: all 0.25s ease-out;
+  height: 5rem;
+  width: 100%;
 `
 
-
+const StyledHeader = styled.div`
+  width: 100%;
+  padding: 0;
+  z-index: 20;
+`
+const ActionButton = styled.div`
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  justify-content: 'center';
+  align-items: 'center';
+  margin: 0;
+`
 
 //ctrl + space gives you the options of props for destructure
 const Header: React.FC = () => {
   //type defined in an interface
   const [elephant, setElephant] = useState<string | boolean | null>('yes')
 
-  return (<Fragment>
-  <StyledHeader />
-  </Fragment>
+  return (
+    <Fragment>
+      <StyledWrapper>
+        <ActionButton>
+          <FontAwesomeIcon icon={faBars} style={{ color: '#FFF' }} size="2x" />
+        </ActionButton>
+      </StyledWrapper>
+    </Fragment>
     //hovering over ref and onChange will tell you htmlelement it expects
-
   )
 }
 
 export default Header
+;
---
+++ src/config/theme.tsx
@@ -0,0 +1,2 @@
+export const mainBlack: string = '#000'
+export const mainWhite: string = '#FFF'
